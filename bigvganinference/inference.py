aa1b9235d8128f36778bf8bb8dc32fed5e5af3a8
thunn <tom_hunn@me.com>
2024-11-12 08:44:56 +0000

    lint

--- bigvganinference/inference.py
+++ bigvganinference/inference.py
@@ -1,10 +1,10 @@
+from enum import Enum
+
 import numpy as np
 import torch
-from bigvganinference.env import AttrDict
-from enum import Enum
-from bigvganinference.bigvgan import BigVGAN
 
-from typing import Dict, Optional, Union
+from bigvganinference.bigvgan import BigVGAN
+from bigvganinference.env import AttrDict
 from bigvganinference.meldataset import get_mel_spectrogram
 
 
